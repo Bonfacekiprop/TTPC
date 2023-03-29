6c2e7a43999a113cfb4891fc97411e99e3fbde6a
elidakirigo <elidakirigo@gmail.com>
2023-03-29 16:59:34 +0300

    adding home component

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,13 +3,11 @@ import { StyledTitle, StyledSubTitle} from "../componenets/style";
 const Home = () => {
     return (
         <div>
-            <div></div>
           <StyledTitle size={65}>
             WElcome to TTPC
           </StyledTitle>
           <StyledSubTitle size={27}>
             Feel free to explore our page 
-
           </StyledSubTitle>
           
         </div>
